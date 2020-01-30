0e2a6a16c3918f65446e452b5fd5f3285d79953a
Eduardo0515 <183382@ids.upchiapas.edu.mx>
2020-01-30 14:47:06 -0600

    first commit

---
+++ .venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\hugoru~1\docume~1\polite~1\quinto~1\progra~1\primer~1\django\venv~1\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

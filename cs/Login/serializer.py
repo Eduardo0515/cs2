a3bd1cc03bfac65646e87e5461494bed0efcc9c6
Eduardo0515 <183382@ids.upchiapas.edu.mx>
2020-02-10 15:28:17 -0600

    Subiendo modelo, get y post

---
+++ cs/Login/serializer.py
@@ -0,0 +1,10 @@
+#------------AGTEGANDO LIBRERIAS FRAMEWORK----------------
+from rest_framework import routers, serializers, viewsets
+
+#----------AGREGANDO MODELOS------------
+from Login.models import Example2
+
+class Example2Serializers(serializers.ModelSerializer):
+    class Meta: 
+        model = Example2
+        fields = ('__all__')
--- cs/Login/urls.py
+++ cs/Login/urls.py
@@ -3,8 +3,10 @@ from django.conf.urls import include
 from django.contrib.auth.models import User
 # from rest_framework import routers, serilizers, viewsets
 from Login.views import CustonAuthToken
+from Login import views
 
 urlpatterns = [
-    re_path(r'^', CustonAuthToken.as_view()),
+    re_path(r'Login/$', CustonAuthToken.as_view()),
     #Hola soy Hugo
+    re_path(r'example_Lista2/$',views.ExampleList2.as_view()),    
 ]

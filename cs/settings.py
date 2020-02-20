e12cdafa7340f53e84e83cefd16f6eb09bef6f57
Eduardo0515 <183382@ids.upchiapas.edu.mx>
2020-02-19 21:01:02 -0600

    agregando DNS

--- cs/settings.py
+++ cs/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = [config('LOCAL'),config('IP')]
+ALLOWED_HOSTS = [config('LOCAL'),config('IP'),config('DNS')]
 # Application definition
 
 INSTALLED_APPS = [
